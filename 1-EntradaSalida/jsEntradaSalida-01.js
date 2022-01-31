ecba0e67f516ce028f35ddc98ff318ea5e720e6c
unknown <alaan.pf@gmail.com>
2022-01-31 10:24:57 -0300

    Ejercicio 1

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -1,8 +1,11 @@
 /*
+Alan Porco Fagalde DIV D
+Ejercicio N°1
 Al presionar el  botón, 
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("ok");
+	alert("Esto funciona de maravilla");
+	alert("Ejercicio N°1")
 }
 
